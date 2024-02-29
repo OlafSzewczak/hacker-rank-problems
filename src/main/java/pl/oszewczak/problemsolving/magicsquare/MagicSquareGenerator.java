b8996d4273d2fc6a90e2cf8be57b6afd8bb1f056
OlafSzewczak <szewczak.olaf@gmail.com>
2024-02-29 20:43:06 +0100

    Rename module, add main method, delete classes for another problem

--- src/main/java/pl/oszewczak/problemsolving/magicsquare/MagicSquareGenerator.java
+++
@@ -1,9 +0,0 @@
-package pl.oszewczak.problemsolving.magicsquare;
-
-import java.util.List;
-
-public interface MagicSquareGenerator {
-
-    List<List<List<Integer>>> generateAllValidSquares();
-
-}
